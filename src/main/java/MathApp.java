e420c0b879283ce6722b0bb29cafe1b1436ad7ee
jrvent <jventura@appdev.yearup.org>
2023-04-12 15:30:07 -0400

    MathEquation

--- src/main/java/MathApp.java
+++ src/main/java/MathApp.java
@@ -1,3 +1,13 @@
-public class MathApp {
+import org.w3c.dom.ls.LSOutput;
 
+public class MathApp {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 10;
+        int c = 8;
+        int result;
+
+        result = a - c;
+        System.out.println(results);
+    }
 }
